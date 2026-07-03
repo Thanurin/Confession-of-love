50b7a8235cb02a03cc8c6ca5f0f40e6a2edfc64b
Nu <Thanurin8@gmail.com>
2026-07-03 13:14:26 +0700

    Add files via upload

---
+++ prank/javascript.js
@@ -0,0 +1,6 @@
+document.querySelector('.submit').addEventListener('click', function() {
+    window.location.href = 'relationship.html';
+});
+document.querySelector('.cancel').addEventListener('click', function() {
+    alert('The "No" button is disabled! Why won\'t you try to love me back? :( ');
+});
