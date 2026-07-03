870642fa584bc7b7d9d1220f32fa6ad5cad0b14c
Nu <Thanurin8@gmail.com>
2026-07-03 13:34:34 +0700

    Delete prank directory

--- prank/javascript.js
+++
@@ -1,6 +0,0 @@
-document.querySelector('.submit').addEventListener('click', function() {
-    window.location.href = 'relationship.html';
-});
-document.querySelector('.cancel').addEventListener('click', function() {
-    alert('The "No" button is disabled! Why won\'t you try to love me back? :( ');
-});
